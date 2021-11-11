df1137c30eaad78415fd80efc15e358f35176d03
LustrilloMendozaEduardoArturo <Lustrillomendozaeduardoarturo@gmail.com>
2021-11-11 08:27:01 -0600

    Initial commit

---
+++ app/src/main/java/com/Eduardo/Lustrillo/Mendoza/MainActivity.java
@@ -0,0 +1,14 @@
+package com.Eduardo.Lustrillo.Mendoza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
